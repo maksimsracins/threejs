412d737c1b30e82bdd6c6703bff1f2c879e7d75a
maksimsracins <56410320+maksimsracins@users.noreply.github.com>
2025-06-09 22:30:45 +0300

    09 - Debug UI to be continued...

--- threejs/src/script.js
+++ threejs/src/script.js
@@ -1,34 +1,46 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import GUI from 'lil-gui'
+
+const gui = new GUI();
 
 /**
  * Base
- */
+*/
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const geometry = new THREE.BufferGeometry()
-
-const count = 500
-const positionsArray = new Float32Array(count * 3 * 3)
-
-for(let i = 0; i < count * 3 * 3; i++){
-    positionsArray[i] = (Math.random() - 0.5) * 4
-}
-
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
-geometry.setAttribute('position', positionsAttribute);
-
-const material = new THREE.MeshBasicMaterial({ color: 0xf12222, wireframe: true})
+/**
+ * Object
+*/
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+gui.add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
 
-// Sizes
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+gui.addColor(material, 'color')
+    .onChange(() => console.log('value changed'))
+
+// let myObject = {
+//     myVariable: 1337
+// }
+
+// gui.add(myObject, 'myVariable')
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -49,23 +61,32 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
