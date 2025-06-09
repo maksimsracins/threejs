91f2ea8da6516c15e7fb5bd5f62f20f5970e65be
maksimsracins <56410320+maksimsracins@users.noreply.github.com>
2025-06-09 15:22:43 +0300

    06 - Cameras

--- threejs/src/script.js
+++ threejs/src/script.js
@@ -1,61 +1,92 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-//Scene
-const scene = new THREE.Scene()
+console.log(OrbitControls)
 
-//Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-const axesHelper = new THREE.AxesHelper(2);
-
-
-
-scene.add(mesh)
-scene.add(axesHelper)
-
-//Sizes
-const sizes = {
-  width: 800,
-  height: 600
+const cursor = {
+    x: 0,
+    y: 0
 }
 
-//Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+window.addEventListener('mousemove', (event) => {
+    // console.log('Mouse moved:', event.clientX, event.clientY)
+     cursor.x = event.clientX / sizes.width - 0.5
+     cursor.y = -(event.clientY / sizes.height - 0.5)
 
-camera.position.z = 3
 
-scene.add(camera)
 
-//Canvas
+})
+
+/**
+ * Base
+ */
+// Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-//Renderer
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+//const camera = new THREE.OrthographicCamera(
+//    -1, 1, 1, -1, 0.1, 100
+//)
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 5
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.target.y = 1
+controls.update()
+
+// Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-//const clock = new THREE.Clock()
-gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
-gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
+// Animate
+const clock = new THREE.Clock()
 
-//Animations
-const loop = () => {
+const tick = () =>
+{
+    // const elapsedTime = clock.getElapsedTime()
 
-  //Time
-  //const elapsedTime = clock.getElapsedTime()
+    //Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position) 
 
-  //Update object
-  //camera.position.y = Math.sin(elapsedTime)
-  //camera.position.x = Math.cos(elapsedTime)
-  //camera.lookAt(mesh.position)
+    // Update objects
+    // mesh.rotation.y = elapsedTime;
 
-  //Render
-  renderer.render(scene, camera)
+    //Update
+    controls.update()
 
-  window.requestAnimationFrame(loop);
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
-loop()
+tick()
