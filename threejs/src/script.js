3cf7bdad8d54ec4cabb7c249be5f488b70ae0ca4
maksimsracins <56410320+maksimsracins@users.noreply.github.com>
2025-06-30 22:17:46 +0300

    15 - Shadows

--- threejs/src/script.js
+++ threejs/src/script.js
@@ -2,6 +2,14 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 
+
+const textureLoader = new THREE.TextureLoader()
+const bakedShadowTexture = textureLoader.load('/textures/bakedShadow.jpg')
+bakedShadowTexture.colorSpace = THREE.SRGBColorSpace
+
+const simpleShadowTexture = textureLoader.load('/textures/simpleShadow.jpg')
+
+
 /**
  * Base
  */
@@ -14,83 +22,114 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1)
-gui.add(ambientLight, 'intensity').min(0).max(10).step(0.01).name('Ambient Light Intensity')
-ambientLight.color = new THREE.Color(0xFFFFFF)
-//scene.add(ambientLight)
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
+gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
+scene.add(ambientLight)
 
-const directionalLight = new THREE.DirectionalLight(0xfffff, 1)
-directionalLight.position.set(1, 0.25, 0)
-gui.add(directionalLight, 'intensity').min(0).max(10).step(0.01).name('Directional Light Intensity')
-//scene.add(directionalLight)
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
+directionalLight.position.set(2, 2, - 1)
+gui.add(directionalLight, 'intensity').min(0).max(3).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 6
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.radius = 10
 
-const hemisphereLight = new THREE.HemisphereLight(0x00ff00, 0x0000ff, 1)
-scene.add(hemisphereLight)
- 
-const pointLight = new THREE.PointLight(0xff0000, 1, 10)
-pointLight.position.set(1, 0, 1)
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+directionalLight.shadow.camera 
+directionalLightCameraHelper.visible = false
+scene.add(directionalLight, directionalLightCameraHelper)
+
+const spotLight = new THREE.SpotLight(0xffffff, 0.3, 10, Math.PI * 0.3)
+spotLight.castShadow = true
+spotLight.shadow.mapSize.width = 1024
+spotLight.shadow.mapSize.height = 1024
+spotLight.shadow.camera.near = 1
+spotLight.shadow.camera.far = 6
+spotLight.position.set(0,2,2)
+scene.add(spotLight)
+scene.add(spotLight.target)
+
+const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+//scene.add(spotLightCameraHelper)
+//spotLight.visible = false
+
+spotLight.position.set(0, 2, 2)
+//scene.add(spotLightCameraHelper)
+
+const pointLight = new THREE.PointLight(0xffffff,2.7)
+pointLight.castShadow = true
+pointLight.position.set(-1, 1, 0)
+pointLight.shadow.mapSize.height = 1024
+pointLight.shadow.mapSize.width = 1024
+pointLight.shadow.camera.near = 0.1
+pointLight.shadow.camera.far = 6
+
+
 scene.add(pointLight)
 
-const rectAreaLight = new THREE.RectAreaLight(0x00ff00, 1, 1, 2)
-rectAreaLight.position.set(0, 1, 0)
-rectAreaLight.lookAt(new THREE.Vector3(0, 0, 0))
-gui.add(rectAreaLight, 'intensity').min(0).max(10).step(0.01).name('Rect Area Light Intensity')
-scene.add(rectAreaLight)
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+pointLightCameraHelper.visible = false
+scene.add(pointLightCameraHelper)
 
-const spotLight = new THREE.SpotLight(0x0000ff, 1, 10, Math.PI * 0.2, 0.25, 1)
-scene.add(spotLight)
 
-// Helpers
-
-const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
-hemisphereLightHelper.visible = true // Set to true to see the helper
-scene.add(hemisphereLightHelper)
-
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
-directionalLightHelper.visible = true // Set to true to see the helper
-scene.add(directionalLightHelper)
-
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
-pointLightHelper.visible = true // Set to true to see the helper
-scene.add(pointLightHelper)
-
+/**
+ * Materials
+ */
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
 
 /**
  * Objects
  */
-// Material
-const material = new THREE.MeshStandardMaterial()
-material.roughness = 0.4
-
-// Objects
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 32, 32),
     material
 )
-sphere.position.x = - 1.5
 
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.75, 0.75, 0.75),
-    material
-)
-
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 32, 64),
-    material
-)
-torus.position.x = 1.5
+sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(5, 5),
-    material
+    new THREE.MeshBasicMaterial({
+        map: bakedShadowTexture
+    })
 )
 plane.rotation.x = - Math.PI * 0.5
-plane.position.y = - 0.65
+plane.position.y = - 0.5
 
-scene.add(sphere, cube, torus, plane)
+plane.receiveShadow = true
+
+scene.add(sphere, plane)
+
+const sphereShadow = new THREE.Mesh(
+    new THREE.PlaneGeometry(1.5,1.5),
+    new THREE.MeshBasicMaterial({
+        color:0x000000,
+        transparent: true,
+        alphaMap: simpleShadowTexture
+    })
+)
+sphereShadow.rotation.x = -Math.PI * 0.5
+sphereShadow.position.y = plane.position.y + 0.01
+
+scene.add(sphereShadow)
 
 /**
  * Sizes
@@ -137,6 +176,12 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+
+renderer.shadowMap.type  = THREE.PCFShadowMap
+
+renderer.shadowMap.enabled = false
+
 
 /**
  * Animate
@@ -147,14 +192,13 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update objects
-    sphere.rotation.y = 0.1 * elapsedTime
-    cube.rotation.y = 0.1 * elapsedTime
-    torus.rotation.y = 0.1 * elapsedTime
+    sphere.position.x = Math.cos(elapsedTime) * 1.5
+    sphere.position.z = Math.sin(elapsedTime) * 1.5
+    sphere.position.y = Math.abs(Math.sin(elapsedTime * 3))
 
-    sphere.rotation.x = 0.15 * elapsedTime
-    cube.rotation.x = 0.15 * elapsedTime
-    torus.rotation.x = 0.15 * elapsedTime
+    sphereShadow.position.x = sphere.position.x
+    sphereShadow.position.z = sphere.position.z
+    sphereShadow.material.opacity = (1 - sphere.position.y) * 0.6
 
     // Update controls
     controls.update()
